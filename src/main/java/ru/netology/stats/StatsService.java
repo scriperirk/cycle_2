697ca91c2b76e58df110a9007fffb32f67c75540
Scriper.irk <scriper.irk@gmail.com>
2022-03-06 01:33:32 +0800

    initial commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,12 +1,68 @@
 package ru.netology.stats;
 
 public class StatsService {
+    //Считаем сумму всех продаж.
     public int calculateSum(int[] sales) {
         int sum = 0;
         for (int sale : sales) {
             sum += sale;
         }
         return sum;
+    }
+
+    //Считаем среднюю сумму продаж в месяц
+    public int calculateAmountPerMonth(int[] sales) {
+        return calculateSum(sales) / sales.length;
+    }
+
+    //Номер месяца, в котором был пик продаж
+    public int calculateMonthMax(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    //Номер месяца, в котором был минимум продаж
+    public int calculateMonthMin(int[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    //Кол-во месяцев, в которых продажи были ниже среднего
+    public int calculateTheNumberIsBelowAverage(int[] sales) {
+        int score = 0;
+        int average = calculateSum(sales) / sales.length;
+        for (int sale : sales) {
+            if (sale < average) {
+                score = score+1;
+            }
+        }
+        return score;
+    }
+
+    //Кол-во месяцев, в которых продажи были выше среднего
+    public int calculateTheNumberIsAboveAverage(int[] sales) {
+        int score = 0;
+        int average = calculateSum(sales) / sales.length;
+        for (int sale : sales) {
+            if (sale > average) {
+                score = score+1;
+            }
+        }
+        return score;
     }
 
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,19 +1,74 @@
 package ru.netology.stats;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void shoudCalculateSum() {
         StatsService servise = new StatsService();
-        int[] sales = {18,20,25,35,45};
-        int expected = 143;
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
 
         int actual = servise.calculateSum(sales);
 
-        assertEquals(expected,actual);
-        
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void shoudAmountPerMonth() {
+        StatsService servise = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = servise.calculateAmountPerMonth(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateMonthMax() {
+        StatsService servise = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = servise.calculateMonthMax(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateMonthMin() {
+        StatsService servise = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = servise.calculateMonthMin(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateTheNumberIsBelowAverage() {
+        StatsService servise = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = servise.calculateTheNumberIsBelowAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateTheNumberIsAboveAverage() {
+        StatsService servise = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = servise.calculateTheNumberIsAboveAverage(sales);
+
+        assertEquals(expected, actual);
+    }
 }
